e87e0a75153c246475426e9413a85312f5614bb6
wahib-git <wahibbachoua95@gmail.com>
2025-11-04 13:39:18 +0100

    Initial Commit

---
+++ social-media-app/social_media_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  social_media_appApp.swift
+//  social-media-app
+//
+//  Created by mac 3 on 4/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct social_media_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
